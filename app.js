c7c67695c01cce25d06effe7822e21ad83dabaaa
Andrea <ggandream24@gmail.com>
2026-06-12 18:36:58 -0600

    feat: mobile version

--- app.js
+++ app.js
@@ -7,4 +7,16 @@ console.log($dropdown);
 $menuBtn.addEventListener("click", () => {
   $dropdown.classList.toggle("hidden");
   $menuBtn.classList.toggle("menu-btn--close");
+
+  const isHidden = $menuBtn.getAttribute("aria-hidden") === "true";
+  $menuBtn.setAttribute("aria-hidden", !isHidden);
+});
+
+document.addEventListener("click", (e) => {
+  if (!e.target.closest(".menu-btn")) {
+    $dropdown.classList.add("hidden");
+    $menuBtn.classList.remove("menu-btn--close");
+
+    $menuBtn.setAttribute("aria-hidden", String(true));
+  }
 });
