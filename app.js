25397692bb1cb342442b0f95305cab80d7164f17
Andrea <ggandream24@gmail.com>
2026-07-27 14:32:14 -0600

    feat: delete console.log

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const $menuBtn = document.querySelector(".menu-btn");
 const $dropdown = document.querySelector(".dropdown");
 
-console.log($menuBtn);
-console.log($dropdown);
-
 $menuBtn.addEventListener("click", () => {
   $dropdown.classList.toggle("hidden");
   $menuBtn.classList.toggle("menu-btn--close");
